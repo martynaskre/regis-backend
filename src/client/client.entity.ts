4ffe77dd79af87897f21dde52b3de8909cea07d2
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2021-10-27 22:39:24 +0300

    edit on imports

--- src/client/client.entity.ts
+++ src/client/client.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class User {
