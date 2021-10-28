97c2047e8857a7688532b5f9d155e47a3d8f2001
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2021-10-28 17:53:17 +0300

    created auth module

--- src/client/dto/CreateClient.dto.ts
+++ src/client/dto/CreateClient.dto.ts
@@ -17,15 +17,6 @@ export class CreateClientDto extends LogInClientDto {
   @IsString()
   lastName: string;
 
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @MinLength(8)
-  password: string;
-
   @IsOptional()
   @IsPhoneNumber()
   phoneNumber: string;
