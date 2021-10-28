dc7298476b700b8601e8d2b62d8d645151b2fe19
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2021-10-28 16:43:07 +0300

    Fixed db set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,12 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(<string>process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
