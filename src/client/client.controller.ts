83793b90c98243a0c256d3eaaf6e3b9f02f34be9
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2021-11-05 15:08:01 +0200

    Implemented signup for client

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,11 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
 import { ClientService } from './client.service';
 import { CreateClientDto } from './dto/create-client.dto';
+import { ValidationPipe } from '../shared/pipes/validation.pipe';
+import { formatResponse } from '../utils';
 
-@Controller('client')
+
+@Controller('clients')
 export class ClientController {
-  constructor(private clientService: ClientService) {}
+  constructor(
+    private clientService: ClientService,
+    private readonly authService: AuthService
+    ) {}
+   
+  @Post('signup')
+  @UsePipes(new ValidationPipe())
+  async signup(@Body() clientData: CreateClientDto) {
+    await this.clientService.create(clientData);
 
+    return formatResponse('Client created.');
+  }  
  
-  }
 }
+
