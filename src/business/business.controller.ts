34a2395ef8ed1f8c0c9896638d29f7c4a37855fb
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2021-11-08 16:14:18 +0200

    initial endpoints for business

--- src/business/business.controller.ts
+++ src/business/business.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { ProviderGuard } from '../auth/guards/provider.guard';
 import { RequestWithProvider } from 'src/interfaces/requestWithProvider.interface';
 import { BusinessService } from './business.service';
 import { CreateBussinesDto } from './dto/create-business.dto';
@@ -8,9 +9,20 @@ export class BusinessController {
   constructor(private readonly bussinesService: BusinessService) {}
 
   @Post()
+  @UseGuards(ProviderGuard)
   async createPost(@Body() business: CreateBussinesDto,
   @Req() request: RequestWithProvider) {
     console.log(request.provider)
     return this.bussinesService.create(business, request.provider);
   }
+
+  //@POST(/service)
+
+  //@PUT(:id)
+
+  //@GET()
+
+  //@GET(:id)
+
+  //@DELETE()
 }
