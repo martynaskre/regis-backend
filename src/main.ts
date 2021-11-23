6ee6c6789872252ec1bbeda80c203a742397cf9b
Martynas <martynaskrebe@gmail.com>
2021-11-23 12:47:13 +0200

    added server port to env

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { ValidationPipe } from './shared/pipes/validation.pipe';
 import getLogLevels from './utils/getLogLevels';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {logger: getLogLevels(process.env.NODE_ENV === 'production')});
+  const app = await NestFactory.create(AppModule, {
+    logger: getLogLevels(process.env.NODE_ENV === 'production'),
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
