f7e0e3282e90b20967f147bbbdb0b9f7a1e7c22a
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2021-12-16 10:16:39 +0200

    Update on logic

--- src/booking/clientBooking.service.ts
+++ src/booking/clientBooking.service.ts
@@ -44,10 +44,9 @@ export class ClientBookingService {
       if (
         bookings[x].reservedTime.toISOString() ===
           bookingData.reservedTime.toISOString() ||
-        (bookings[x].reservedTime.getTime() + bookings[x].duration * 3600000 >
-          bookingData.reservedTime.getTime() &&
-          bookings[x].reservedTime.getTime() + bookings[x].duration <
-            bookingData.reservedTime.getTime()) 
+        (bookings[x].reservedTime.getTime() + bookings[x].duration  * 3600000 > bookingData.reservedTime.getTime() 
+        && bookings[x].reservedTime.getTime() < bookingData.reservedTime.getTime()
+        ) 
       ) {
         throwDuplicateBooking({ reservedTime: 'This time is already booked' });
       }
