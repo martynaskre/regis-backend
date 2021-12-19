5c3091a974b4f386624cf4a14c09c6279d32c156
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2021-12-19 15:40:59 +0200

    Installed uuid

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -4,9 +4,10 @@ import { ClientBooking } from '../booking/clientBooking.entity';
 import { PasswordResetEntity } from '../auth/passwor-resets/password-reset.entity';
 import { CleanPasswordResetsJob } from './jobs/clean-password-resets.job';
 import { RateBusiness } from './jobs/rate-email.job';
+import { MailService } from 'src/mail/mail.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PasswordResetEntity,ClientBooking])],
-  providers: [CleanPasswordResetsJob, RateBusiness],
+  providers: [CleanPasswordResetsJob, RateBusiness, MailService],
 })
 export class CronModule {}
--- src/cron/jobs/rate-email.job.ts
+++ src/cron/jobs/rate-email.job.ts
@@ -1,7 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ClientBooking } from 'src/booking/clientBooking.entity';
+import { ClientBooking } from '../../booking/clientBooking.entity';
+import { MailService } from '../../mail/mail.service';
+import { FrontEndpoint } from '../../types';
+import { formatFrontUrl } from '../../utils';
 import { Repository } from 'typeorm';
 
 
@@ -12,24 +15,42 @@ export class RateBusiness {
   constructor(
     @InjectRepository(ClientBooking)
     private readonly clientBookingsRepository: Repository<ClientBooking>,
+    private readonly mailService: MailService,
+
   ) {}
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(CronExpression.EVERY_10_MINUTES)
   async run() {
     this.logger.log('Running rate business email cron job.');
 
     const clientBookings = await this.clientBookingsRepository.createQueryBuilder('clientBooking')
     .where('DATE_ADD(clientBooking.reservedTime, INTERVAL clientBooking.duration HOUR) < :time', {time: new Date()})
+    .andWhere('clientBooking.isNotified = :isNotified' ,{isNotified: false})
+    .leftJoinAndSelect('clientBooking.client', 'client')
     .getMany();
 
-    console.log(clientBookings)
-    console.log( new Date())
+    //sukurti uuid
 
-  }
+    for(let x = 0; x < clientBookings.length; x++)
+    {
+        // await this.mailService.sendMail(clientBookings[x].client.email,
+        //     'Sveikiname prisijungus!',
+        //     'bussines-rating',
+        //     {
+        //       firstName: clientBookings[x].client.firstName,
+        //       actionUrl: formatFrontUrl(FrontEndpoint.CLIENT_LOGIN),
+        //     },
+        // )
+        //clientBookings[x].isNotified = true;
+    }
+}   
 }
 
-
-// FIX CRON JOB
-// ADD FIELD TO CLIENT BOOKING isNotified
-// GET ALL CLIENT BOOKINGS WHERE DATE.NOW > RESERVED TIME + DURATION AND !== isNotified
 // SEND EMAILS TO THEM
+//      loop through all users
+//      get user by its id
+//      send email with user.email
+//          create tamplate
+//          put link to create business endpoint
+//          uuid
+// SET isNotified TO true
